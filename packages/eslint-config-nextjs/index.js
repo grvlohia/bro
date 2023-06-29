ce536bbd518d9528fca465cc5c87cb70579dbc3e
Gaurav Lohia <glohia@tweddle.com>
2023-06-29 12:23:28 +0530

    updating packages

--- packages/eslint-config-nextjs/index.js
+++ packages/eslint-config-nextjs/index.js
@@ -1,11 +1,11 @@
 module.exports = {
-  extends: ['next', 'turbo', '../../.eslintrc.js'],
+  extends: ['next/core-web-vitals', 'turbo', '../../.eslintrc.js'],
   rules: {
-    '@next/next/no-html-link-for-pages': 'off',
+    '@next/next/no-html-link-for-pages': 'off'
   },
   parserOptions: {
     babelOptions: {
-      presets: [require.resolve('next/babel')],
-    },
-  },
+      presets: [require.resolve('next/babel')]
+    }
+  }
 }
